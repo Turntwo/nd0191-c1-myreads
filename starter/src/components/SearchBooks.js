20a2ff1b2e0d2c27ca6f8d6ddc58f283d024dc81
Jason Christian <jason.christian@jefbar.com>
2022-05-24 18:15:34 -0700

    feat: Implement Routing

---
+++ starter/src/components/SearchBooks.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const SearchBooks = () => {
+
+    return (
+
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link
+                className="close-search"
+                to="/"
+              >
+                Close
+              </Link>
+              <div className="search-books-input-wrapper">
+                <input
+                  type="text"
+                  placeholder="Search by title, author, or ISBN"
+                />
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+    );
+}
+
+SearchBooks.propTypes = {
+
+}
+
+export default SearchBooks;
